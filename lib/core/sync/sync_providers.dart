2f08057d41d0794b303aacef9f231cbb5ba310bc
jaykayhq <ukpaijk@outlook.com>
2026-05-13 15:06:33 +0100

    feat: add provider for sync trigger service

--- lib/core/sync/sync_providers.dart
+++ lib/core/sync/sync_providers.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:emerge_app/core/drift/database.dart';
+import 'package:emerge_app/core/services/connectivity_service.dart';
 import 'package:emerge_app/core/sync/sync_engine.dart';
+import 'package:emerge_app/core/sync/sync_trigger_service.dart';
 
 final enhancedSyncEngineProvider = Provider<EnhancedSyncEngine>((ref) {
   final mutationQueue = ref.watch(mutationQueueDaoProvider);
@@ -11,4 +13,19 @@ final enhancedSyncEngineProvider = Provider<EnhancedSyncEngine>((ref) {
 final pendingSyncCountProvider = StreamProvider<int>((ref) {
   final dao = ref.watch(mutationQueueDaoProvider);
   return Stream.periodic(const Duration(seconds: 2), (_) => dao.getAllPending().then((l) => l.length)).asyncMap((f) => f);
+});
+
+final syncTriggerServiceProvider = Provider<SyncTriggerService>((ref) {
+  final syncEngine = ref.watch(enhancedSyncEngineProvider);
+  final service = SyncTriggerService(
+    syncEngine,
+    (ConnectivityListener listener) {
+      ref.listen(connectivityStreamProvider, (_, next) {
+        next.whenData((results) => listener(results));
+      });
+    },
+  );
+  ref.onDispose(() => service.stop());
+  service.start();
+  return service;
 });
