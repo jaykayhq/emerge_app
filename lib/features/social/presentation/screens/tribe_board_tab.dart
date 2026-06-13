ba9b84dc6f542cf66f7e8c70bc68353cc2c76138
jaykayhq <ukpaijk@outlook.com>
2026-06-13 22:23:15 +0100

    feat: add Board and Discover tabs to Tribe Space

---
+++ lib/features/social/presentation/screens/tribe_board_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TribeBoardTab extends StatelessWidget {
+  const TribeBoardTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Leaderboard goes here", style: TextStyle(color: Colors.white)));
+  }
+}
