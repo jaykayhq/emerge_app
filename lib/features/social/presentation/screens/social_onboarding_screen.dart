d363aa9c2bfe68095d2392ca3a6dedf62d093489
jaykayhq <ukpaijk@outlook.com>
2026-06-13 22:17:29 +0100

    feat: update router for new social lobby and onboarding screens

---
+++ lib/features/social/presentation/screens/social_onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SocialOnboardingScreen extends StatelessWidget {
+  const SocialOnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Social Onboarding Screen')),
+    );
+  }
+}
---
+++ lib/features/social/presentation/screens/tribe_space_scaffold.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TribeSpaceScaffold extends StatelessWidget {
+  const TribeSpaceScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Tribe Space Scaffold')),
+    );
+  }
+}
