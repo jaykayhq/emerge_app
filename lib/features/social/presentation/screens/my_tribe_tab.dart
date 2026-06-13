6bcaf7e9c8a83d22ef262cf38d89772328daf77e
jaykayhq <ukpaijk@outlook.com>
2026-06-13 22:22:01 +0100

    feat: add Feed and My Tribe tabs

---
+++ lib/features/social/presentation/screens/my_tribe_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyTribeTab extends StatelessWidget {
+  const MyTribeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("My Tribe content goes here", style: TextStyle(color: Colors.white)));
+  }
+}
---
+++ lib/features/social/presentation/screens/tribe_feed_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TribeFeedTab extends StatelessWidget {
+  const TribeFeedTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Feed content goes here", style: TextStyle(color: Colors.white)));
+  }
+}
