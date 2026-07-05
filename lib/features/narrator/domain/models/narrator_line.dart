63dbbf511199bc4e6ca4cec629224ddf2efedcc3
Kimchi <kimchi@local>
2026-07-05 05:48:32 +0100

    feat(narrator): add NarratorLine sealed class (GenericLine, PersonalLine)

---
+++ lib/features/narrator/domain/models/narrator_line.dart
@@ -0,0 +1,19 @@
+/// A line of narrator text.
+///
+/// [GenericLine] is pre-written copy shown to all users (free tier).
+/// [PersonalLine] is data-grounded copy shown to Pro users.
+sealed class NarratorLine {
+  const NarratorLine();
+}
+
+class GenericLine extends NarratorLine {
+  final String text;
+  const GenericLine(this.text);
+}
+
+class PersonalLine extends NarratorLine {
+  final String text;
+  final String dataBasis;
+
+  const PersonalLine({required this.text, required this.dataBasis});
+}
