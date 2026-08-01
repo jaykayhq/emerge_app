40e3b63c5a2dafb6c6ba0abd491e888a4d41d118
Kimchi <kimchi@local>
2026-08-01 13:20:28 +0100

    test(social): drop companion mock from challenges screen test (API removed in companion cleanup)

--- test/features/social/presentation/screens/challenges_screen_test.dart
+++ test/features/social/presentation/screens/challenges_screen_test.dart
@@ -1,28 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:emerge_app/features/social/presentation/screens/challenges_screen.dart';
 import 'package:emerge_app/features/social/presentation/providers/challenge_bundle_provider.dart';
 import 'package:emerge_app/features/social/domain/models/challenge_bundle.dart';
-import 'package:emerge_app/features/companion/data/repositories/companion_repository.dart';
-import 'package:emerge_app/features/companion/presentation/providers/companion_providers.dart';
-
-class MockCompanionRepository extends Mock implements CompanionRepository {}
-
 class _MockChallengeBundle extends ChallengeBundle {
   @override
   Future<ChallengeBundleData> build() async => ChallengeBundleData.empty();
 }
 
 void main() {
-  late MockCompanionRepository mockCompanionRepo;
-
-  setUp(() {
-    mockCompanionRepo = MockCompanionRepository();
-    when(() => mockCompanionRepo.hasVisited(any())).thenReturn(true);
-  });
-
   Future<void> setScreenSize(tester) async {
     tester.view.physicalSize = const Size(400, 1200);
     tester.view.devicePixelRatio = 1.0;
@@ -36,7 +23,6 @@ void main() {
     return ProviderScope(
       overrides: [
         challengeBundleProvider.overrideWith(() => _MockChallengeBundle()),
-        companionRepositoryProvider.overrideWithValue(mockCompanionRepo),
       ],
       child: const MaterialApp(home: ChallengesScreen(showAppBar: false)),
     );
